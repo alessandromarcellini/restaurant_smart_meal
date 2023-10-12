35a54853ecc2f324b3ab70b7fd30e27a960bcbbf
alessandromarcellini <alessandromarcellini49@gmail.com>
2023-10-12 11:41:48 +0200

    Model almost completed, added tabels: Restaurant, Orde, Client, Plate, Section, Ingredient, Allergen

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import *
+
+
 # Register your models here.
+
+admin.site.register(Restaurant)
+admin.site.register(Order)
+admin.site.register(Client)
+admin.site.register(Plate)
+admin.site.register(Section)
+admin.site.register(Ingredient)
+admin.site.register(Allergen)
